21f14748ce344e9fa91ff8457a899ab37c8c0cc3
gaohuijue <gaohuijue@shinow.com>
2017-12-20 09:43:12 +0800

    build中添加period picker jquery插件代码，并增加示例。

--- jquery.datetimepicker.js
+++ jquery.datetimepicker.js
@@ -21,14 +21,6 @@ var datetimepickerFactory = function ($) {
 				],
 				dayOfWeek: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 			},
-			ch: { // Simplified Chinese
-				months: [
-					"一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"
-				],
-				dayOfWeekShort: [
-					"日", "一", "二", "三", "四", "五", "六"
-				]
-			},
 			zh: { //Simplified Chinese (简体中文)
 				months: [
 					"一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"
